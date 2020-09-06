4d1ff052376a6ca1809cb93c550085869c510cc7
Sachin <Sachin@HP>
2020-09-06 16:46:58 +0530

    first commit DB configurations

---
+++ SpringBoot-All-in-One-DataJpa/src/test/java/com/ingecno/app/SpringBootAllInOneDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingecno.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAllInOneDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
